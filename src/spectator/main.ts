9fbe8f0d7c16494fa314883b5ff5bf6e43cfea06
Aaron Nech <necha@cs.washington.edu>
2015-05-13 17:06:25 -0700

    fixed

--- src/spectator/main.ts
+++ src/spectator/main.ts
@@ -4,6 +4,6 @@ var AppComponent : any = require('./component/AppComponent.jsx');
 React.render(
     React.createElement(AppComponent, 
     	{
-    		 'API' : 'ws://pokerbot.herokuapp.com:1337'
+    		 'API' : 'ws://pokerbot.herokuapp.com'
     	}),
     document.getElementById('content'));
