cb0273d19b6605476218f50f66b5976b9e84b98f
Aaron Nech <necha@cs.washington.edu>
2015-05-15 05:25:53 -0700

    Fixed

--- src/server/engine/lib/utils.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports.protectArrayBet = function(table, index, protectedBet) {
-    for (var i = 0; i <= index; i++) {
-        table[i] = table[i] || 0;
-    }
-    table[index] = table[index] + protectedBet;
-    return table;
-};
